771d5b5486648d9ab32c9b455d48cefc77e9abef
swarit <dholakia.swarit@gmail.com>
2023-06-13 21:57:24 -0400

    Update AboutPage.js

--- src/media/AboutPage.js
+++ src/media/AboutPage.js
@@ -9,7 +9,7 @@ const AboutPage = () => {
 
 I’m a 22-year-old from Toronto deeply interested in startups that'll solve hard, non-obvious problems and impact billions using exponential technologies, and play to spend my life building such. I’m currently interested in global freight, primary healthcare, and consumer fintech.
 
-Lately, I’ve also been exploring other areas, like: the relations between technology and public policy, modern constitutional and antitrust law frameworks, economic global development in emerging markets and borderless digital value transfer.
+Lately, I’ve also been exploring other areas, like: the relations between technology and public policy, modern constitutional and antitrust law frameworks, economic development in emerging markets and borderless digital value transfer.
 
 I’m an aspiring triathlete and marathonist, and enjoy skiing, backpacking and Nolan movies.
 
@@ -19,19 +19,17 @@ I’m an aspiring triathlete and marathonist, and enjoy skiing, backpacking and 
 
 **->** Graduated from Western University, where I studied Software Engineering
 
-**->** Had new-grad SWE roles fall through, and am re-exploring areas I truly want to spend my time in: one day I'll start a company or join one at the early-stage, but right now I'm very interested in venture studios as a way to build for problems with breadth
+**->** Am re-exploring areas I truly want to spend my time working in: right now, I'm very interested in venture studios as a way to build for problems with breadth
 
 **->** Exploring unintentional technology concequences of legacy policy in the US
 
 **->** Learning about institutional and growth investing (and becoming more interested in the idea of money as an tool for impact)
 
-**->** Studying the stories and intracies of non-technology companies, founders and CEOs, and investors (some examples: LVMH, Tiger Global, Reliance, Jamie Dimon, Berkshire, etc)
+**->** Studying the stories and intracies of non-technology companies, founders and CEOs, and investors (some examples: LVMH, Tiger Global, Bob Iger, Reliance, Jamie Dimon, Berkshire, etc)
 
 **->** Training for my first marathon and hanging out with friends
 
 *(Updated June 13 2023)*
-
-
 
   `;
   
