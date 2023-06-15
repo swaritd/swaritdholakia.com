493b3146dda366bcc708047f42e5b4405fb51199
Swarit Dholakia <dholakia.swarit@gmail.com>
2023-06-15 01:53:12 -0400

    Update AboutPage.js (#17)

--- src/media/AboutPage.js
+++ src/media/AboutPage.js
@@ -7,9 +7,9 @@ const AboutPage = () => {
 ` 
 **Hi! I'm Swarit.**
 
-I’m a 22-year-old from Toronto deeply interested in startups that'll solve hard, non-obvious problems and impact billions using exponential technologies. I plan to spend my life building such. I’m currently interested in global freight, primary healthcare, and consumer fintech.
+I’m a 22-year-old from Toronto deeply interested in startups that'll solve hard, non-obvious problems and impact billions using exponential technologies. 
 
-Lately, I’ve also been exploring other areas: the intersection of technology and public policy, modern antitrust law frameworks, economic development in emerging markets and borderless digital value transfer.
+I’m currently interested in global freight, primary healthcare, and consumer fintech. I’ve also been exploring: the intersection of technology and public policy, modern antitrust law frameworks, economic development in emerging markets and borderless digital value transfer.
 
 I’m an aspiring triathlete and marathonist, and enjoy skiing, backpacking and Nolan movies.
 
@@ -17,19 +17,19 @@ I’m an aspiring triathlete and marathonist, and enjoy skiing, backpacking and 
 
 *Currently,*
 
-**->** Graduated from Western University, where I studied Software Engineering
+**->** Graduated from studying software engineering at Western University
 
-**->** Am re-discovering areas I truly want to spend my time working in: right now, I'm very interested in venture studios as a way to build for problems with breadth
+**->** Re-discovering areas I truly want to spend my time in: currently very interested in venture studios as a way to build for problems with breadth
 
-**->** Exploring unintentional technology concequences of legacy policy in the US
+**->** Exploring unintentional technology concequences of legacy policy
 
 **->** Learning about institutional and growth investing (and becoming more interested in the idea of money as an tool for impact)
 
-**->** Studying the stories and intracies of non-technology companies, founders and CEOs, and investors (some examples: LVMH, Jamie Dimon, Reliance)
+**->** Studying the stories of non-technology companies, leaders, and investors (some examples: LVMH, Jamie Dimon, Reliance)
 
 **->** Training for my first marathon and hanging out with friends
 
-*(Updated June 13 2023)*
+*(Updated June 15 2023)*
 
   `;
   
