5075b5baeaa40f6e93d8dce7b1e97f18c6ad4fc3
Swarit Dholakia <dholakia.swarit@gmail.com>
2024-01-04 18:42:43 -0500

    restructure app to be link-aware

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,77 +1,53 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from '../components/NavigationBar';
 import '../css/App.css';
 import '../css/styles.css';
-import headshot from '../assets/headshot.jpeg';
+
+import Writings from './Writings';
+import Work from './Work';
+import Home from './About';
+
 import twittericon from '../assets/twittericon.png';
 import emailicon from '../assets/emailicon.png';
 import linkedinicon from '../assets/linkedinicon.png';
-import Writings from './Writings';
-import Reads from './Reads';
-import Values from './Values';
-import Work from './Work';
-import Notes from './Notes';
-import About from './About';
-import ClickableText from '../components/ClickableText';
 
 function App() {
 
-  const [selectedComponent, setSelectedComponent] = useState('About');
-  const handleMenuClick = (componentName) => {
-    setSelectedComponent(componentName);
-  };
-  
   return (
 
-    <div className="container">
+    <Router>
+      <div className="App">
+        <NavigationBar />
+        <hr className="horizontal-line" />
+      </div>
 
-      <div className="left">
-        <div>
+      <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '30px', marginTop: '10px' }}>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/writings" element={<Writings/>} />
+          <Route path="/work" element={<Work/>} />
+        </Routes>
+      </div>
 
-          <img src={headshot} alt='headshot' style={{ width: '150px', height: 'auto' }}></img>
+      <hr className="horizontal-line" style={{paddingBottom: '10px'}} />
 
-          <h2 className='text'>Swarit Dholakia</h2>
-
-          <hr className="horizontal-line" />
-
-          <ul>
-            <ClickableText text="About" onClick={() => handleMenuClick('About')} />
-          </ul>
-
-          <ul>
-            <ClickableText text="Writings" onClick={() => handleMenuClick('Writings')} />
-          </ul>
-
-          <ul>
-            <ClickableText text="Work" onClick={() => handleMenuClick('Work')} />
-          </ul>
-
-          <hr className="horizontal-line" />
-
-          <a href="https://twitter.com/swaritdholakia" target="_blank" rel="noopener noreferrer">
+      <div style={{ display: 'flex', justifyContent: 'center', paddingBottom: '10px' }}>
+        <a href="https://twitter.com/swaritdholakia" target="_blank" rel="noopener noreferrer">
           <img src={twittericon} alt='twitterlogo' style={{ width: '25px', height: 'auto' }}></img>
-          </a>
+        </a>
           
-          <a href="mailto:dholakia.swarit@gmail.com" target="_blank" rel="noopener noreferrer">
+        <a href="mailto:dholakia.swarit@gmail.com" target="_blank" rel="noopener noreferrer">
           <img src={emailicon} alt='emailicon' style={{ width: '35px', height: 'auto' }}></img>
-          </a>
+        </a>
           
-          <a href="https://linkedin.com/in/swaritdholakia/" target="_blank" rel="noopener noreferrer">
+        <a href="https://linkedin.com/in/swaritdholakia/" target="_blank" rel="noopener noreferrer">
           <img src={linkedinicon} alt='linkedinicon' style={{ width: '25px', height: 'auto' }}></img>
-          </a>
-
-        </div>
-      </div>
-      
-      <div className='right'>
-        {selectedComponent === 'About' && <About />}
-        {selectedComponent === 'Writings' && <Writings />}
-        {selectedComponent === 'Reads' && <Reads />}
-        {selectedComponent === 'Values' && <Values />}
-        {selectedComponent === 'Work' && <Work />}
-        {selectedComponent === 'Notes' && <Notes />}
+        </a>
       </div>
 
-    </div>
+    </Router>
+    
 
   );
 }
